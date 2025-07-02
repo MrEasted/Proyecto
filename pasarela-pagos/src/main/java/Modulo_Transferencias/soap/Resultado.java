f989a3beb82f5a73dce27d149d53ed911cc6dc60
gaston <gaston.fernandez@estudiantes.utec.edu.uy>
2025-07-02 19:21:14 -0300

    transfer

---
+++ pasarela-pagos/src/main/java/Modulo_Transferencias/soap/Resultado.java
@@ -0,0 +1,114 @@
+
+package Modulo_Transferencias.soap;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para resultado complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>{@code
+ * <complexType name="resultado">
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="codigo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         <element name="mensaje" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         <element name="referencia" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "resultado", propOrder = {
+    "codigo",
+    "mensaje",
+    "referencia"
+})
+public class Resultado {
+
+    protected String codigo;
+    protected String mensaje;
+    protected String referencia;
+
+    /**
+     * Obtiene el valor de la propiedad codigo.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * Define el valor de la propiedad codigo.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCodigo(String value) {
+        this.codigo = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad mensaje.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    /**
+     * Define el valor de la propiedad mensaje.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setMensaje(String value) {
+        this.mensaje = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad referencia.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getReferencia() {
+        return referencia;
+    }
+
+    /**
+     * Define el valor de la propiedad referencia.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setReferencia(String value) {
+        this.referencia = value;
+    }
+
+}
